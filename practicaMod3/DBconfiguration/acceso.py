2141e0a951e2172372b37ea04e5820e129ce783f
Giovanni Javier Juarez Zavaleta <gzavaleta2022@gmail.com>
2026-06-09 22:27:42 +0000

    Se agregó la función insertar_usuario al script acceso.py. Corrección en Dockerfile.

--- practicaMod3/DBconfiguration/acceso.py
+++ practicaMod3/DBconfiguration/acceso.py
@@ -54,6 +54,40 @@ def obtener_datos_usuario(username, password):
     except Exception as e:
         print("Error de consulta a la base de datos: ",e)
 
+def insertar_usuario(nombre, correo,telefono, fecha_nacimiento, username, password):
+    conn = conectar_db()
+    if not conn:
+        return
+    try:
+        cursor = conn.cursor()
+
+        # Insertar en la tabla usuarios
+        cursor.execute(
+            """
+            INSERT INTO usuarios (nombre, correo, telefono, fecha_nacimiento)
+            VALUES (%s, %s, %s, %s) RETURNING id_usuario;
+            """, (nombre, correo, telefono, fecha_nacimiento)
+        )
+
+        id_usuario = cursor.fetchone()[0]
+        cursor.execute(
+            """
+            INSERT INTO credenciales (id_usuario, username, password_hash)
+            VALUES (%s, %s, %s);
+            """, (id_usuario, username, password)
+        )
+
+        conn.commit()
+        print("Nuevo usuario insertado correctamente.")
+
+    except Exception as e:
+        print("Error al insertar el nuevo usuario: ", e)
+        conn.rollback()
+
+    finally:
+        cursor.close()
+        conn.close()
+
 if __name__ == "__main__":
     print("Inicio de sesión en la base de datos.")
 
@@ -61,3 +95,12 @@ if __name__ == "__main__":
     password = getpass.getpass("Ingrese su contraseña: ")
     
     obtener_datos_usuario(username, password)
+
+    #print("Insertar nuevo usuario")
+    #nombre = input("Ingrese el nombre del nuevo usuario: ")
+    #correo = input("Ingrese el correo del nuevo usuario: ")
+    #telefono = input("Ingrese el teléfono del nuevo usuario: ")
+    #fecha_nacimiento = input("Ingrese la fecha de nacimiento del nuevo usuario (YYYY-MM-DD): ")
+    #username = input("Ingrese el nombre de usuario para el nuevo usuario: ")
+    #password = input("Ingrese la contraseña para el nuevo usuario: ")
+    #insertar_usuario(nombre, correo, telefono, fecha_nacimiento, username, password)
